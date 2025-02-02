4286a03a7d5ae7b92dd42dca205bf2c8b66059be
jvcordeiro-stefanini <jvcordeiro@stefanini.com.br>
2025-02-02 20:28:46 -0300

    att

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,13 +1,14 @@
 <template>
-    <h1>Timeline João & Myn ♥</h1>
+    <h1 class="font1">Timeline João & Myn ♥</h1>
+    <p class="dating_time font1">{{ date }}</p>
     <div v-if="!loading" class="container">
         <div class="scroll">
             <div class="timeline" id="timelineContainer" :style="{'width': `${(images_size + 1) * 240}px`}">
                 <div class="timeline-item" v-for="(img, index) in images">
                     <div class="marker"></div>
                     <div class="timeline-content" :class="index % 2 == 0 ? 'top' : 'bottom'">
-                        <div class="timeline-date">{{ img.date }}</div>
-                        <img :src="data + img.content" alt="fevereiro">
+                        <div class="timeline-date font2">{{ img.date }}</div>
+                        <img :src="data + img.content">
                         <h5>{{ img.phrase }}</h5>
                     </div>
                 </div>
@@ -34,13 +35,15 @@ import Preloader from "../components/Preloader.vue"
 import content from '../../content.json'
 
 export default defineComponent({
-    async setup() {           
+    async setup() {
+        const date = ref()
         const images = ref()
         const images_size = ref()
         const data = ref("data:image/")
         const loading = ref(true)
 
         return {
+            date,
             images,
             images_size,
             data,
@@ -59,13 +62,26 @@ export default defineComponent({
             } catch (error) {
                 console.error(error);
             }
-        }
+        },
     }, 
     beforeMount() {
         this.getImages()
+        setInterval(() => {
+            const start_dating = new Date("2024-03-01T00:11:00")
+            const now = new Date()
+            const dif = now - start_dating
+
+            const seconds_total = Math.floor(dif / 1000);
+            const days = Math.floor(seconds_total / (60 * 60 * 24));
+            const hours = Math.floor((seconds_total % (60 * 60 * 24)) / (60 * 60));
+            const minutes = Math.floor((seconds_total % (60 * 60)) / 60);
+            const seconds = seconds_total % 60;
+
+            this.date = `${days} dias ${hours} horas ${minutes} minutos e ${seconds} segundos juntos...`;
+        }, 1000)
     },
     components: {
         Preloader
-    }
+    },
 })
 </script>
